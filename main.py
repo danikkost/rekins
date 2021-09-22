6f58e20b718df21681b0d3679bdfa6264a6db35b
danikkost <90185964+danikkost@users.noreply.github.com>
2021-09-22 10:55:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,35 @@ def print_info():
 def lietotaja_ievade():
     
     vards = input('Ieraksti vardu: ')
-    veltijuma_teksts = input('Ievadi veltijuma tekstu kastitei: ')
+    teksts = input('Ievadi veltijuma tekstu kastitei: ')
     platums = input('Cik plata kastite? Noraadi milimetrus. Tikai veseli skaitli!')
     garums = input('Cik  plata garumaa? Noraadi milimetrus.Tikai veseli skaitli!')
     augstums = input('Cik  augsta? Noraadi milimetrus.Tikai veseli skaitli!')
-    kokmateriala_cena = input('Kokmateriala cena')
-    return vards, veltijuma_teksts, platums, garums, augstums, kokmateriala_cena
+    materials = input('Kokmateriala cena')
+    return vards, teksts, platums, garums, augstums, materials
+
+    darba_samaksa = 15
+    PVN = 21
+    produkta_cena = tekst * 1.2 + width / 100 * length / 100 * height / 100 / 3 * material
+    PVN_summa = produkta_cena + darba_samaksa * PVN / 100
+    rekina_summa = produkta_cena + darba_samaksa + PVN_summa
+    print("Produkta cena: €" + format(produkta_cena, ",.2f"))
+    print("PVN summa: €" + format(PVN_summa, ",.2f"))
+    print("rekina_summa: €" + format(rekina_summa, ",.2f"))
+    
+    
+from datetime import date
+today = date.today()
+print("Today's date:", today)
+    
+    	
 
 if __name__ == '__main__':
     print_info()
     while True:
        lietotaja_ievade()
-       run_again = input('Vai velies sagatavot rekinu velreiz? JAA - 1, NEE - 0')
-       if run_again == 1:
-           continue
+       restart = input('Vai velies sagatavot rekinu velreiz? JAA - 1, NEE - 0')
+       if restart == 1:
+           print_info()
        else:
-           exit(0)
+           break
