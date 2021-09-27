b8d744888328f4810f5495301014d522bd739d3f
danikkost <90185964+danikkost@users.noreply.github.com>
2021-09-27 13:26:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,52 @@
 import rekins
 
+
 def print_info():
     print('Programma rekina sagatavosanai')
 
+
 def lietotaja_ievade():
-    
     vards = input('Ieraksti vardu: ')
     teksts = input('Ievadi veltijuma tekstu kastitei: ')
-    platums = input('Cik plata kastite? Noraadi milimetrus. Tikai veseli skaitli!')
-    garums = input('Cik  plata garumaa? Noraadi milimetrus.Tikai veseli skaitli!')
-    augstums = input('Cik  augsta? Noraadi milimetrus.Tikai veseli skaitli!')
+    platums = float(input('Cik plata kastite? Noraadi milimetrus. Tikai veseli skaitli!'))
+    garums = float(input('Cik  plata garumaa? Noraadi milimetrus.Tikai veseli skaitli!'))
+    augstums = float(input('Cik  augsta? Noraadi milimetrus.Tikai veseli skaitli!'))
     materials = input('Kokmateriala cena')
     return vards, teksts, platums, garums, augstums, materials
 
     darba_samaksa = 15
     PVN = 21
-    produkta_cena = tekst * 1.2 + width / 100 * length / 100 * height / 100 / 3 * material
+    produkta_cena = teksts * 1.2 + platums / 100 * garums / 100 * augstums / 100 / 3 * materials
     PVN_summa = produkta_cena + darba_samaksa * PVN / 100
     rekina_summa = produkta_cena + darba_samaksa + PVN_summa
+
     print("Produkta cena: €" + format(produkta_cena, ",.2f"))
     print("PVN summa: €" + format(PVN_summa, ",.2f"))
     print("rekina_summa: €" + format(rekina_summa, ",.2f"))
-    
-    f = open("rekins.txt", "w")
-    f.write()
-    f.close()    	    
+
+    guy = [format(produkta_cena),format(PVN_summa),format(rekina_summa)]
+
+    f = open("rekins.txt", "x")
+    f.write("\n")
+    f.write("Produkta cena: " + format(produkta_cena, ",.2f") + "€")
+    f.write("\n")
+    f.write("PVN summa: " + format(PVN_summa, ",.2f")+ "€")
+    f.write("\n")
+    f.write("rekina_summa: " + format(rekina_summa, ",.2f")+ "€")
+    f.close()
+
+
 from datetime import date
+
 today = date.today()
 print("Today's date:", today)
-    
-   
 
 if __name__ == '__main__':
     print_info()
     while True:
        lietotaja_ievade()
-       restart = input('Vai velies sagatavot rekinu velreiz? JAA - 1, NEE - 0')
-       if restart == 1:
+       restart = input('Gribat vel?').lower()
+       if restart == "yes":
            print_info()
        else:
            break
