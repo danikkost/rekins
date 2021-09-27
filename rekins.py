d138d662573c14b66211399ec973b0d9bb4126d5
danikkost <90185964+danikkost@users.noreply.github.com>
2021-09-27 13:26:32 +0300

    Update rekins.py

--- rekins.py
+++ rekins.py
@@ -1,12 +1,10 @@
 
-
 class Rekins:
 
-    def __init__(self,vards, teksts, garums, platums,augstums, materials) -> object:
+    def __init__(self,vards, teksts, garums, platums,augstums, materials) :
         self.vards = vards
         self.teksts = teksts
         self.garums = garums
         self.platums = platums
         self.augstums = augstums
         self.materials = materials
-        laiks = datetime.datetime.now()
